fd46e8886a1a91e01ff047a65d28cc2e1f2b25e2
capthapl <dawidkacprzak@icloud.com>
2019-10-21 10:20:40 +0200

    Added try catch to test vmcontroller

--- OyaHost.BLL/Universal/VmController.cs
+++ OyaHost.BLL/Universal/VmController.cs
@@ -7,12 +7,17 @@ namespace OyaHost.BLL.Universal
     {
         public static void GetAllVmNames(){
             //var response = BashController.ExecCommand("sudo virsh list --all | awk '{ print $2 }' | tail -n +3");
-
-            string[] names = new string[999];
-            Connect.ListDefinedDomains(IntPtr.Zero, ref names, 999);
-            foreach (var item in names)
+            try
             {
-                Console.WriteLine(item);
+                string[] names = new string[999];
+                Connect.ListDefinedDomains(IntPtr.Zero, ref names, 999);
+                foreach (var item in names)
+                {
+                    Console.WriteLine(item);
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
             }
         }
     }
